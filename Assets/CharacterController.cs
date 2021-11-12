b6493619e0bd7b3a2867fa048160393d64578b34
TScre <tscreciu2021@elam.co.uk>
2021-11-12 11:52:35 +0000

    code

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -4,28 +4,34 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-
-    public float maxSpeed = 1.0f;
+    public float maxSpeed = 3.0f;
     float rotation = 0.0f;
-    float camRotation = 0.0f;
+    public float camRotaion = 1.0f;
     GameObject cam;
-
+    float rotaionSpeed = 2.0f;
+    public float camRotaionSpeed = 1.5f;
+    Rigidbody myRidgidbody;
+   
     // Start is called before the first frame update
     void Start()
     {
-      cam = GameObject.Find("Main Camera"); 
+        cam = GameObject.Find("Main Camera");
+        myRidgidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-
     void Update()
     {
-        
-    transform.position = transform.position + (transform.forward * Input.GetAxis("Vertical") * maxSpeed);
+        isOnGround = Physics. CheckSphere(groundChecker.transform.position, 0.1f, ground );
 
-    rotation = rotation + Input.GetAxis("Mouse X");
-    transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
+        myRidgidbody.velocity = new Vector3(newVelocity.x, myRidgidbody.velocity.y, newVelocity.z);
 
-    camRotation = camRotation + Input.GetAxis("Mouse Y");
+        rotation = rotation + Input.GetAxis("Mouse X") * rotaionSpeed;
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        camRotaion = camRotaion + Input.GetAxis("Mouse Y") * camRotaionSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotaion, 0.0f, 0.0f));
+
     }
 }
