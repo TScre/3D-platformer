6ec6617e483bf9ac48a2cd86ce979701b9fac8e8
TScre <tscreciu2021@elam.co.uk>
2021-11-16 14:49:50 +0000

    fixed errors added jump

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float maxSpeed = 3.0f;
-    float rotation = 0.0f;
-    public float camRotaion = 1.0f;
+    public float camRotationSpeed = 1.5f;
+    public float camRotation = 1.0f;
     GameObject cam;
-    float rotaionSpeed = 2.0f;
-    public float camRotaionSpeed = 1.5f;
+    float rotation = 0.0f;
+    float rotationSpeed = 2.0f;
     Rigidbody myRidgidbody;
-   
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+    public float jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,16 +27,20 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isOnGround = Physics. CheckSphere(groundChecker.transform.position, 0.1f, ground );
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        if (isOnGround == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            myRidgidbody.AddForce(transform.up * jumpForce);
+        }
 
         Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
         myRidgidbody.velocity = new Vector3(newVelocity.x, myRidgidbody.velocity.y, newVelocity.z);
 
-        rotation = rotation + Input.GetAxis("Mouse X") * rotaionSpeed;
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed;
         transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
 
-        camRotaion = camRotaion + Input.GetAxis("Mouse Y") * camRotaionSpeed;
-        cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotaion, 0.0f, 0.0f));
-
+        camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
     }
 }
