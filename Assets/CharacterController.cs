594e3391ce8fa8c37705641c3571401afed995c6
TScre <tscreciu2021@elam.co.uk>
2021-11-17 12:41:19 +0000

    started on moving platform

--- Assets/CharacterController.cs
+++ Assets/CharacterController.cs
@@ -5,11 +5,15 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour
 {
     public float maxSpeed = 3.0f;
+    public float normalSpeed = 10.0f;
+    public float sprintSpeed = 20.0f;
+
     public float camRotationSpeed = 1.5f;
     public float camRotation = 1.0f;
     GameObject cam;
     float rotation = 0.0f;
     float rotationSpeed = 2.0f;
+    
     Rigidbody myRidgidbody;
 
     bool isOnGround;
@@ -17,11 +21,16 @@ public class CharacterController : MonoBehaviour
     public LayerMask groundLayer;
     public float jumpForce;
 
+    public float maxSprint = 5.0f;
+    float sprintTimer;
+
     // Start is called before the first frame update
     void Start()
     {
         cam = GameObject.Find("Main Camera");
         myRidgidbody = GetComponent<Rigidbody>();
+
+        sprintTimer = maxSprint;
     }
 
     // Update is called once per frame
@@ -34,6 +43,20 @@ public class CharacterController : MonoBehaviour
             myRidgidbody.AddForce(transform.up * jumpForce);
         }
 
+        if (Input.GetKey(KeyCode.LeftShift) && sprintTimer > 0.0f)
+        {
+            maxSpeed = sprintSpeed;
+            sprintTimer = sprintTimer - Time.deltaTime;
+        }else
+        {
+            maxSpeed = normalSpeed;
+            if (Input.GetKey(KeyCode.LeftShift) == false){
+                sprintTimer = sprintTimer + Time.deltaTime;
+            }
+        }
+
+        sprintTimer = Mathf.Clamp(sprintTimer, 0.0f, maxSprint);
+
         Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed + (transform.right * Input.GetAxis("Horizontal") * maxSpeed);
         myRidgidbody.velocity = new Vector3(newVelocity.x, myRidgidbody.velocity.y, newVelocity.z);
 
@@ -42,5 +65,7 @@ public class CharacterController : MonoBehaviour
 
         camRotation = camRotation + Input.GetAxis("Mouse Y") * camRotationSpeed;
         cam.transform.localRotation = Quaternion.Euler(new Vector3(-camRotation, 0.0f, 0.0f));
+
+
     }
 }
